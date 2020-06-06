87069c3698ac66f1213abd0b0e8fe1a969be259c
Bandham Manikanta <bandham.manikanta@centurylink.com>
2020-06-06 13:18:28 +0530

    '**'

--- BeautifulSoup/flipkart-product-details-app/app.py
+++ BeautifulSoup/flipkart-product-details-app/app.py
@@ -7,11 +7,10 @@ app = Flask(__name__)
 BASE_URL = 'https://www.flipkart.com';
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
     app.run()
     
-@app.route('/urls', methods=['GET'])
-def getProductUrls():
+@app.route('/products', methods=['GET'])
+def getProductsDetails():
     keyword = request.args.get('q')
     response = makeHttpCallGetResponse(keyword)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -44,106 +43,24 @@ def getProductUrls():
         name_price_tag = prod_soup.find('h1',{'class':'_9E25nV'})
         name = ''
         for span in name_price_tag.find_all('span'):
-            name = name + ' ' + span.text
+            name = name + span.text
         product['name'] = name
         product['price'] = prod_soup.find('div',{'class':'_1vC4OE _3qQ9m1'}).text
         
-        product['noOfRatingsAndReviews'] = prod_soup.find('span',{'class':'_38sUEc'}).text
+        rev_rat_tag = prod_soup.find('span',{'class':'_38sUEc'})
+        
+        if rev_rat_tag is not None:
+            product['noOfRatingsAndReviews'] = rev_rat_tag.text
+        else:
+            if 'Be the first to Review this product' in prod_soup.find('span',{'class':'_1IcGRZ'}).text:
+                product['noOfRatingsAndReviews'] = '0 ratings & 0 reviews';
+        
+        product['link'] = link
         
         products.append(product)
         
     #return make_response(jsonify({'Products':products, 'Count':len(products), 'Urls': urls}), 200)
     return make_response(jsonify({'Products':products, 'count':len(products)}), 200)
-
-@app.route('/products', methods=['GET'])
-def getProducts():
-    keyword = request.args.get('q')
-    response = makeHttpCallGetResponse(keyword)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    urls = set()
-    
-    for a in soup.find_all('a',{'class':'_3dqZjq'}):
-        urls.add(a.get('href'))
-    
-    for a in soup.find_all('a',{'class':'_2cLu-l'}):
-        urls.add(a.get('href'))
-        
-    for a in soup.find_all('a',{'class':'_31qSD5'}):
-        urls.add(a.get('href'))
-        
-    print('no of product links found', len(urls))
-    
-    # extracting product details
-    products = list()
-    for link in urls:
-        product = dict()
-        prod_soup = BeautifulSoup(requests.get(link).text,'html.parser')
-    
-        # name, price, review count & ratings count
-        name_price_tag = prod_soup.find('div',{'class':'_29OxBi'})
-        name = ''
-        for span in name_price_tag.find_all('span'):
-            name = name + span.text
-        product['name'] = name
-        product['price'] = prod_soup.find('div',{'class':'_1vC4OE _3qQ9m1'}).text
-        
-        # TODO Improve
-        noOfRatings = None
-        noOfReviews = None
-        try:
-            rev_rat_tag = prod_soup.find('span',{'class':'_38sUEc'}).span.find_all('span')
-            noOfRatings = rev_rat_tag[0].text
-            noOfReviews = rev_rat_tag[2].text
-        except Exception as e:
-            print(e)
-            noOfRatings = ''
-            noOfReviews = ''
-        product['no.of review'] = noOfRatings
-        product['no.of ratings'] = noOfReviews
-        
-        # highlights
-        try:
-            highlights_tags = prod_soup.find('div',{'class':'_3WHvuP'}).find_all('li',{'class':'_2-riNZ'})
-            highlights = [fea.text for fea in highlights_tags]
-            product['highlights'] = highlights
-        except Exception as e:
-            print(e)
-            product['highlights'] = list()
-    
-        # specifications
-        specs_box_tag = prod_soup.find('div',{'class':'MocXoX'})
-        specs = dict()
-        if specs_box_tag is not None:
-            specs_tag = specs_box_tag.find_all('div',{'class':'_2RngUh'})
-            for spec_tag in specs_tag:
-                td_tags = [tr.find_all('td') for tr in spec_tag.find_all('tr')]
-                for td in td_tags:
-                    if len(td)==2:
-                        specs[td[0].text] = td[1].text
-        else:
-            specs = {}
-        product['specifications'] = specs
-    
-        # reviews
-        reviews = list()
-        review_tags_parent = prod_soup.find_all('div',{'class':'_3nrCtb'})
-        for rev_tag in review_tags_parent:
-            try:
-                review = dict()
-                review['rating'] = rev_tag.find('div',{'class':'hGSR34 E_uFuv'}).text
-                review['title'] = rev_tag.find('p',{'class':'_2xg6Ul'}).text
-                review['message'] = rev_tag.find('div',{'class':'qwjRop'}).text
-                review['customerName'] = rev_tag.find('p',{'class':'_3LYOAd _3sxSiS'}).text
-                reviews.append(review)
-            except Exception as e:
-                print(e)
-                pass
-            
-        product['reviews'] = reviews
-        products.append(product)
-
-    print(products)
-    return make_response(jsonify({'products':products}), 200)
 
 def makeHttpCallGetResponse(keyword):
     headers = {
